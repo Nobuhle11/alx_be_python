b0ccdbf0604ac08307dec197546246b7a9807df7
Nobuhle11 <wayithinobuhle@gmail.com>
2025-06-08 19:21:52 +0200

    Fix perform_operation signature to pass checks

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Perform a basic arithmetic operation based on the input.
-
-    Parameters:
-    - num1 (float): First number
-    - num2 (float): Second number
-    - operation (str): Operation to perform ('add', 'subtract', 'multiply', 'divide')
-
-    Returns:
-    - float or str: Result of the operation or an error message
-    """
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
